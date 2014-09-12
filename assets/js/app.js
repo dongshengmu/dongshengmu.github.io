8e09c0490ed34008829171c04998c37320177520
Dongsheng Mu <Dongsheng@taos-air.gateway.pace.com>
2014-09-12 01:10:10 -0700

    new template

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+define(['jquery', 'domReady', 'tweenlite', 'easing', 'easeljs', 'cover', 'feeds'], function ($, domReady, tweenlite, easing, easeljs, cover, feeds) {
+  domReady(function () {
+
+    if ($('.frame').length){
+      cover.init( $('.frame') );
+    }
+
+    if ($('.instagram, .twitter').length){
+      feeds.init();
+    }
+
+
+
+  });
+});
